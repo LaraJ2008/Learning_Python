2c931d19d190eb6b6b9ff94115c9f601d423f7f8
LaraJ2008 <laraj2008@gmail.com>
2023-01-30 16:54:49 -0700

    sum to 100 working

--- Loops_upon_loops.py
+++ Loops_upon_loops.py
@@ -4,22 +4,19 @@
 #produce incremented number then add it to Partial_sum, assign Partial_sum to Full_sum?
 #really this is just increment, add, increment, add...
 Base_number = 0
-Top_number = 10
+Top_number = 100
+Current_addend = 1
 
 
-Current_addend = Base_number + 1 
+
 while Current_addend <= Top_number:
     if Current_addend == 1:
         Partial_sum = 1
-        Current_addend +=1
+        Current_addend += 1
     else:
-        Full_sum = Current_addend + Partial_sum 
-        Current_addend +=1
-        print(Full_sum)
-
-#Base = 0 current_addend = Base + 1, sum = 1
-
-#increment current_addend 
-#current addend + sum = Sum
+        Partial_sum = Current_addend + Partial_sum           
+        Current_addend += 1
+print(Partial_sum)
+        
 
 
